7f092ea12a196d9a0a6dd771de61344a8a80609e
Oliver Michel <oliver.michel@colorado.edu>
2019-03-12 09:03:22 -0600

    array for rings handled by current file descriptor

--- include/netmap_tools.h
+++ include/netmap_tools.h
@@ -5,237 +5,269 @@
 #include <stdexcept>
 
 namespace netmap {
-	
-	class iface
-	{
-		class ring
-		{
-		public:
-			ring() = delete;
-			explicit ring(netmap_ring* ring_)
-				: _ring(ring_) { }
 
-			ring(const ring&) = delete;
-			ring& operator=(const ring&) = delete;
+    class iface
+    {
+        class ring
+        {
+        public:
+            ring() = delete;
+            explicit ring(netmap_ring* ring_)
+                    : _ring(ring_) { }
 
-			ring(ring&&) = default;
-			ring& operator=(ring&&) = default;
-			
-			inline unsigned count_slots() const
-			{
-				return _ring->num_slots;
-			}
+            ring(const ring&) = delete;
+            ring& operator=(const ring&) = delete;
 
-			inline unsigned buffer_size() const
-			{
-				return _ring->nr_buf_size;
-			}
+            ring(ring&&) = default;
+            ring& operator=(ring&&) = default;
 
-			inline unsigned head() const
-			{
-				return _ring->head;
-			}
+            inline unsigned count_slots() const
+            {
+                return _ring->num_slots;
+            }
 
-			inline unsigned tail() const
-			{
-				return _ring->tail;
-			}
+            inline unsigned buffer_size() const
+            {
+                return _ring->nr_buf_size;
+            }
 
-			inline unsigned current() const
-			{
-				return _ring->cur;
-			}
+            inline unsigned head() const
+            {
+                return _ring->head;
+            }
 
-			inline bool avail() const
-			{
-				return _ring->cur != _ring->tail;
-			
-			}
+            inline unsigned tail() const
+            {
+                return _ring->tail;
+            }
 
-			~ring()
-			{
-				_ring = nullptr;
-			}
+            inline unsigned current() const
+            {
+                return _ring->cur;
+            }
 
-		protected:
-			netmap_ring* _ring;
-		};
+            inline bool avail() const
+            {
+                return _ring->cur != _ring->tail;
 
-		class tx_ring : public ring
-		{
-		public:
-			explicit tx_ring(netmap_ring* ring_)
-				: ring(ring_) { }
+            }
 
-			char* next_buf()
-			{
-				return _ring->cur != _ring->tail ?
-					NETMAP_BUF(_ring, _ring->slot[_ring->cur].buf_idx) : nullptr;
-			}
+            ~ring()
+            {
+                _ring = nullptr;
+            }
 
-			void advance(unsigned pkt_len_)
-			{
-				unsigned i = _ring->cur;
-				_ring->slot[i].len = (uint16_t) pkt_len_;
-				_ring->head = _ring->cur = nm_ring_next(_ring, i);
-			}
-		};
+        protected:
+            netmap_ring* _ring;
+        };
 
-		class rx_ring : public ring
-		{
-		public:
-			explicit rx_ring(netmap_ring* ring_)
-				: ring(ring_) { }
+        class tx_ring : public ring
+        {
+        public:
+            explicit tx_ring(netmap_ring* ring_)
+                    : ring(ring_) { }
 
-			char* next_buf(unsigned& len_)
-			{
-				if (_ring->cur != _ring->tail) {
-					len_ = _ring->slot[_ring->cur].len;
-					return NETMAP_BUF(_ring, _ring->slot[_ring->cur].buf_idx);
-				} else return nullptr;
-			}
+            char* next_buf()
+            {
+                return _ring->cur != _ring->tail ?
+                       NETMAP_BUF(_ring, _ring->slot[_ring->cur].buf_idx) : nullptr;
+            }
 
-			void advance()
-			{
-				unsigned i = _ring->cur;
-				_ring->head = _ring->cur = nm_ring_next(_ring, i);
-			}
-		};
+            void advance(unsigned pkt_len_)
+            {
+                unsigned i = _ring->cur;
+                _ring->slot[i].len = (uint16_t) pkt_len_;
+                _ring->head = _ring->cur = nm_ring_next(_ring, i);
+            }
+        };
 
-		class _ring_proxy
-		{
-		public:
-			explicit _ring_proxy(const iface& iface_)
-				: _iface(iface_) { }
+        class rx_ring : public ring
+        {
+        public:
+            explicit rx_ring(netmap_ring* ring_)
+                    : ring(ring_) { }
 
-			inline const std::vector<unsigned>& fd_rings() const
-			{
-				return _fd_rings;
-			}
+            char* next_buf(unsigned& len_)
+            {
+                if (_ring->cur != _ring->tail) {
+                    len_ = _ring->slot[_ring->cur].len;
+                    return NETMAP_BUF(_ring, _ring->slot[_ring->cur].buf_idx);
+                } else return nullptr;
+            }
 
-		protected:
-			const iface& _iface;
-			std::vector<unsigned> _fd_rings;
-		};
+            void advance()
+            {
+                unsigned i = _ring->cur;
+                _ring->head = _ring->cur = nm_ring_next(_ring, i);
+            }
+        };
 
-		class _tx_ring_proxy : public _ring_proxy
-		{
-		public:
-			explicit _tx_ring_proxy(const iface& iface_)
-				: _ring_proxy(iface_)
-			{
-				for (unsigned i = 0; i < _iface.tx_rings.count(); i++)
-					if (rx_ring(NETMAP_TXRING(_iface._nmd->nifp, i)).buffer_size() > 0)
-						_fd_rings.push_back(i);
-			}
-			
-			tx_ring operator[](unsigned i_)
-			{
-				if (i_ >= _iface.tx_rings.count())
-					throw std::logic_error("netmap::iface: invalid tx ring id "
-					+ std::to_string(i_));
+        class _ring_proxy
+        {
+        public:
+            _ring_proxy() = default;
 
-				return tx_ring(NETMAP_TXRING(_iface._nmd->nifp, i_));
-			}
+            explicit _ring_proxy(iface* iface_)
+                    : _iface(iface_) { }
+
+            inline const std::vector<unsigned>& fd_rings() const
+            {
+                return _fd_rings;
+            }
+
+            inline void set_iface(iface* iface_)
+            {
+                _iface = iface_;
+            }
+
+        protected:
+            iface* _iface = nullptr;
+            std::vector<unsigned> _fd_rings;
+        };
+
+        class _tx_ring_proxy : public _ring_proxy
+        {
+        public:
+
+            _tx_ring_proxy() = default;
+
+//			explicit _tx_ring_proxy(iface* iface_)
+//				: _ring_proxy(iface_)
+//			{
+//			    if (_iface->_nmd == nullptr)
+//			        std::cout << "_tx_ring: is null" << std::endl;
+//			}
+
+            tx_ring operator[](unsigned i_)
+            {
+                if (i_ >= _iface->tx_rings.count())
+                    throw std::logic_error("netmap::iface: invalid tx ring id "
+                                           + std::to_string(i_));
+
+                return tx_ring(NETMAP_TXRING(_iface->_nmd->nifp, i_));
+            }
 
             inline unsigned count() const
             {
-                return _iface._nmd->req.nr_tx_rings;
-            }
-
-			inline void synchronize()
-			{
-				ioctl(_iface._nmd->fd, NIOCTXSYNC);
-			}
-		};
-
-		class _rx_ring_proxy : public _ring_proxy
-		{
-		public:
-			explicit _rx_ring_proxy(const iface& iface_) 
-				: _ring_proxy(iface_)
-			{
-				for (unsigned i = 0; i < _iface.rx_rings.count(); i++)
-					if (rx_ring(NETMAP_RXRING(_iface._nmd->nifp, i)).buffer_size() > 0)
-						_fd_rings.push_back(i);
-			}
-
-			rx_ring operator[](unsigned i_)
-			{
-				if (i_ >= _iface.rx_rings.count())
-					throw std::logic_error("netmap::iface: invalid rx ring id "
-					+ std::to_string(i_));
-				
-				return rx_ring(NETMAP_RXRING(_iface._nmd->nifp, i_));
-			}
-
-			inline unsigned count() const
-            {
-                return _iface._nmd->req.nr_rx_rings;
+                return _iface->_nmd->req.nr_tx_rings;
             }
 
             inline void synchronize()
-			{
-				ioctl(_iface._nmd->fd, NIOCRXSYNC);
-			}
-		};
-	
-	public:
-/*
-		static unsigned count_tx_rings(const std::string& iface_name_)
-		{
-			auto nmd = _open(iface_name_);
-			auto nr_tx_rings = nmd->req.nr_tx_rings;
-			_close(nmd);
-			return nr_tx_rings;
-		}
+            {
+                ioctl(_iface->_nmd->fd, NIOCTXSYNC);
+            }
 
-		static unsigned count_rx_rings(const std::string& iface_name_)
-		{
-			auto nmd = _open(iface_name_);
-			auto nr_rx_rings = nmd->req.nr_rx_rings;
-			_close(nmd);
-			return nr_rx_rings;
-		}
-*/
-		explicit iface(const std::string& iface_name_)
-			: _nmd(_open(iface_name_)), tx_rings(*this), rx_rings(*this) { }
+//			void _init()
+//            {
+//                for (unsigned i = 0; i < _iface->tx_rings.count(); i++)
+//					if (rx_ring(NETMAP_TXRING(_iface->_nmd->nifp, i)).buffer_size() > 0)
+//						_fd_rings.push_back(i);
+//            }
+        };
 
-		int fd() const
-		{
-			return _nmd->fd;
-		}
+        class _rx_ring_proxy : public _ring_proxy
+        {
+        public:
+            _rx_ring_proxy() = default;
 
-		~iface()
-		{
-			nm_close(_nmd);
-		}
+//		    explicit _rx_ring_proxy(iface* iface_)
+//				: _ring_proxy(iface_)
+//			{
+//                if (_iface->_nmd == nullptr)
+//                    std::cout << "_rx_ring: is null" << std::endl;
+//			}
 
-		_tx_ring_proxy tx_rings;
-		_rx_ring_proxy rx_rings;
+            rx_ring operator[](unsigned i_)
+            {
+                if (i_ >= _iface->rx_rings.count())
+                    throw std::logic_error("netmap::iface: invalid rx ring id "
+                                           + std::to_string(i_));
 
-	private:
-		
-		static nm_desc* _open(const std::string& iface_name_)
-		{
-			struct nm_desc base_nmd { };
-			bzero(&base_nmd, sizeof(base_nmd));
-			struct nm_desc* nmd = nm_open(("netmap:" + iface_name_).c_str(), nullptr, 0, &base_nmd);
+                return rx_ring(NETMAP_RXRING(_iface->_nmd->nifp, i_));
+            }
 
-			if (!nmd)
-				throw std::runtime_error("netmap::iface: could not open device " + iface_name_);
+            inline unsigned count() const
+            {
+                return _iface->_nmd->req.nr_rx_rings;
+            }
 
+            inline void synchronize()
+            {
+                ioctl(_iface->_nmd->fd, NIOCRXSYNC);
+            }
+//
+//			void _init()
+//            {
+//                for (unsigned i = 0; i < _iface->rx_rings.count(); i++)
+//                    if (rx_ring(NETMAP_RXRING(_iface->_nmd->nifp, i)).buffer_size() > 0)
+//                        _fd_rings.push_back(i);
+//            }
+        };
 
+    public:
 
-			return nmd;
-		}
+        static unsigned count_tx_rings(const std::string& iface_name_)
+        {
+            auto nmd = _open(iface_name_);
+            auto nr_tx_rings = nmd->req.nr_tx_rings;
+            _close(nmd);
+            return nr_tx_rings;
+        }
 
-		static void _close(nm_desc* nmd_)
-		{
-			nm_close(nmd_);	
-		}
+        static unsigned count_rx_rings(const std::string& iface_name_)
+        {
+            auto nmd = _open(iface_name_);
+            auto nr_rx_rings = nmd->req.nr_rx_rings;
+            _close(nmd);
+            return nr_rx_rings;
+        }
 
-		struct nm_desc* _nmd = nullptr;
-	};
+        explicit iface(const std::string& iface_name_)
+        {
+            _nmd = iface::_open(iface_name_);
+
+            if (_nmd == nullptr)
+                std::cerr << "iface: is null" << std::endl;
+
+            tx_rings.set_iface(this);
+            rx_rings.set_iface(this);
+        }
+
+        int fd() const
+        {
+            return _nmd->fd;
+        }
+
+        ~iface()
+        {
+            nm_close(_nmd);
+        }
+
+        _tx_ring_proxy tx_rings;
+        _rx_ring_proxy rx_rings;
+
+    private:
+
+        static nm_desc* _open(const std::string& iface_name_)
+        {
+//			std::cout << "iface::_open()" << std::endl;
+//
+            struct nm_desc base_nmd { };
+            bzero(&base_nmd, sizeof(base_nmd));
+            struct nm_desc* nmd = nm_open(("netmap:" + iface_name_).c_str(), nullptr, 0, &base_nmd);
+
+            if (nmd == nullptr)
+                throw std::runtime_error("netmap::iface: could not open device " + iface_name_);
+
+            return nmd;
+        }
+
+        static void _close(nm_desc* nmd_)
+        {
+            nm_close(nmd_);
+        }
+
+        struct nm_desc* _nmd = nullptr;
+    };
 }
 
